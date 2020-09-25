2b75443fbe0e35f07a784e25193bab218e747fa4
Исламов Азат Ильхамович <Islamov-AI@nft.local>
2020-09-25 15:48:17 +0500

    FirstVers

---
+++ CSVParse/CSVParser/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace CSVParser
+{
+
+	public static class Program
+	{
+		public class Item
+		{
+			public string Name;
+			public int Price;
+			public int Index;
+
+			public Item(string name, int price, int index)
+			{
+				Name = name;
+				Price = price;
+				Index = index;
+			}
+		}
+
+		public static void Main()
+		{
+			Console.WriteLine("Reading, wait...");
+			var file = File.ReadAllLines("1.csv", Encoding.UTF8);
+
+			Console.WriteLine("Reading ended");
+			Console.WriteLine("Serializing");
+
+			var names = new List<Item>();
+			var buffer = new HashSet<string>();
+
+			var index = 1;
+			var rnd = new Random();
+
+			for (var i = 2895995; i < file.Length - 1; i++)
+			{
+				var tokens = file[i].Split('\t');
+
+				var name = tokens[2];
+				var stripName = "";
+
+				if (name.Length < 5)
+				{
+					stripName = name;
+				}
+				else
+					stripName = name.Remove(4);
+
+				if (buffer.Add(stripName))
+				{
+					names.Add(new Item(name, rnd.Next(1, 1000), index));
+					index++;
+				}
+			}
+
+			Console.WriteLine(names.Count);
+
+			var value = JsonConvert.SerializeObject(names, Formatting.Indented);
+
+			File.WriteAllText("1.json", value, Encoding.UTF8);
+			
+
+			Console.WriteLine("Serializing ended");
+		}
+	}
+}
---
+++ OpenJson/OpenJson/Program.cs
@@ -0,0 +1,51 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+using System.Linq;
+
+namespace CSVParser
+{
+
+	public static class Program
+	{
+
+		public class Item
+		{
+			public string Name;
+			public int Price;
+			public int Index;
+
+			public Item(string name, int price, int index)
+			{
+				Name = name;
+				Price = price;
+				Index = index;
+			}
+		}
+
+		static async Task Main()
+        {
+			var jsonText = File.ReadAllText("1.json");
+
+			var file = JsonConvert.DeserializeObject<List<Item>>(jsonText);
+
+			var files = file.OrderByDescending(x => x.Index).Where(p => p.Price < 100);
+			var files1 = file.OrderBy(x => x.Price).Where(p => p.Price > 100).Where(p => p.Price <200);
+			var files2 = file.OrderBy(x => x.Price).Where(p => p.Price > 200).Where(p => p.Price < 300);
+			var files3 = file.OrderBy(x => x.Price).Where(p => p.Price > 300).Where(p => p.Price < 1000);
+
+			var desc = new[] { files, files1, files2, files3 };
+
+			for (int i=0; i <4; i++)
+			{
+				var value3 = JsonConvert.SerializeObject(desc[i], Formatting.Indented);
+				File.WriteAllText(string.Format("test{0}.txt", i), value3);
+			}
+			Console.WriteLine("Serializing ended");
+		}
+    }
+}
